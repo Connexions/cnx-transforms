9016716e63c14d7a8313ea7a4e50c678f6652e56
Michael Mulich <michael.mulich@gmail.com>
2013-04-09 11:37:36 -0700

    MS Word document to *office document conversion function. Dervived from Products.CNXMLTransforms.

--- cnxtransforms/__init__.py
+++ cnxtransforms/__init__.py
@@ -7,7 +7,13 @@
 # ###
 """Conversion callables"""
 import os
+import logging
+import subprocess
 from io import FileIO
+
+here = os.path.abspath(os.path.dirname(__file__))
+OOCONVERT = os.path.join(here, 'helper-scripts', 'ooconvert.py')
+logger = logging.getLogger('cnxtransforms')
 
 
 class File(FileIO):
@@ -27,3 +33,47 @@ class File(FileIO):
     @property
     def filepath(self):
         return os.path.join(self.basepath, self.filename)
+
+
+def word_to_ooo(file, output=None, server_address=None, page_count_limit=0):
+    """MS Word document to OpenOffice.org (OOo) document.
+
+    :param file: The file object to be converted.
+    :type file: cnxtransforms.Input
+    :param server_address: Host and port of running *Office server
+    :type server_address: string
+    :param page_count_limit: ???
+    :type page_count_limit: integer
+    :returns: The output object used to write to output to.
+    :rtype: cnxtransforms.File
+
+    """
+    # Quote the file name to prevent shell script expansion of any
+    #   special characters in the file name.
+    # Sending office the name of the imported word file (saved locally).
+    command = [OOCONVERT, file.filepath]
+    if server_address:
+        command.extend(['--address', server_address])
+    if page_count_limit:
+        command.extend(['--pagecount', page_count_limit])
+    logger.info("Invoking converter: {}".format(' '.join(command)))
+
+    if output is None:
+        output = File("{}.odt".format(file.filepath), 'w')
+
+    # TODO Send the document as binary via the input pipe,
+    #      like how the result is returned.
+    proc = subprocess.Popen(command,
+                            stdin=subprocess.PIPE,
+                            stdout=output,
+                            stderr=subprocess.PIPE,
+                            )
+    # TODO A timeout parameter was added to communicate in Python 3.3.
+    #      ...communicate(timeout=seconds)
+    junk, stderr = proc.communicate()
+    del junk
+
+    if proc.returncode != 0:
+        raise Exception("Failed with stderr:\n{}".format(stderr))
+
+    return output
