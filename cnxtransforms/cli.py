ec9c1bad64f618f67b416c4041c8f9a965d6d10c
Michael Mulich <michael.mulich@gmail.com>
2013-04-15 11:12:33 -0700

    CNXML -> HTML command-line interface. Introduces the 'python-magic' dependency for mime type lookup of buffers, because the standard libraries' 'mimetypes' only supports file based lookup. This intentionally leaves out support for zip based cnxml to html conversion support.

--- cnxtransforms/cli.py
+++ cnxtransforms/cli.py
@@ -10,7 +10,7 @@ import sys
 import argparse
 
 import cnxtransforms
-
+from cnxtransforms import logger
 
 def init_parser(parser=None):
     """Initialize the commandline argument parser"""
@@ -51,5 +51,50 @@ def soffice2cnxml(args=None, parser=None):
 
     # And now the body of work...
     content = cnxtransforms.odt_to_cnxml(args.source)
+    cnxtransforms.to_zipfile(content, output=args.destination)
+    return 0
+
+def _guess_type(buf):
+    """Like mimetypes.guess_type but accepts a buffer instead of a filepath.
+
+    """
+    import magic
+    magick = magic.Magic(mime=True)
+    return magick.from_buffer(buf.read(1024))
+
+def cnxml2html(args=None, parser=None):
+    """CNXML to HTML
+    XXX Zip input is incomplete
+
+    """
+    parser = init_parser(parser)
+    args = parser.parse_args(args)
+
+    # What type of input is this? CNXML file or ZIP file?
+
+    # FIXME If the content is piped in, we will likely need to read it
+    #       into a string buffer so that we can reread from it.
+    #       'has_rewind_ability(io) == True' =)
+    # XXX This type of guessing game should happen elsewhere, but for
+    #     now this will do.
+    input = args.source
+    if getattr(args.source, 'name') == '<stdin>':
+        input = cnxtransforms.File.from_io(args.source)
+    input_mime_type = _guess_type(input)
+    # Rewind, because the _guess_type function reads into the input.
+    input.seek(0)
+    if input_mime_type == 'application/zip':
+        is_single_file = False
+        ##input = FileSequence.from_zipfile(input)
+        raise NotImplementedError
+    else:
+        is_single_file = True
+
+    if is_single_file:
+        logger.warning("If the input CNXML document contains resources, the "
+                       "output will likely be incomplete.")
+
+    # Do the conversion
+    content = cnxtransforms.cnxml_to_html(input)
     cnxtransforms.to_zipfile(content, output=args.destination)
     return 0
