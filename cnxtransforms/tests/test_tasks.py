9acc05bad58e1a678d13aef39f5f52a68ec95076
Michael Harrison <mwharrison@rice.edu>
2015-09-25 15:12:09 +0000

    add metadata retrieval tests, add encoding

--- cnxtransforms/tests/test_tasks.py
+++ cnxtransforms/tests/test_tasks.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2013, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+import os
 import unittest
 import zipfile
 import pdb
@@ -5,15 +13,39 @@ import pdb
 EPUB = None
 CALLBACK = None
 
+here = os.path.abspath(os.path.dirname(__file__))
+TEST_DATA_DIR = os.path.join(here, 'data')
+
 class MakeEpub(unittest.TestCase):
+    def make_one(self, file):
+        from cnxepub import Package
+        return Package.from_file(file)
+    
     @property
     def target(self):
-        from ..tasks import make_epub
-        return make_epub
+        from ..tasks import make_styled_epub
+        return make_styled_epub
             
-    def test_makeepub(self):
-        epub = "cnxtransforms/tests/data/simultaneous-equations-1.1.epub"
-        better_epub = self.target(epub, CALLBACK)
+    def test_get_metadata(self):
+        package_filepath = os.path.join(
+            TEST_DATA_DIR, 'book',
+            "9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6.opf")
+        package = self.make_one(package_filepath)
+        
+        expected_metadata = {
+            'publisher': "Connexions",
+            'publication_message': u'Nueva Versión',
+            'identifier': "org.cnx.contents.9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6",
+            'title': "9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6",
+            'language': "en-us",
+            'publisher': "Connexions",
+            'license_text': "This work is shared with the public using the Attribution 3.0 Unported (CC BY 3.0) license.",
+            'license_url': "http://creativecommons.org/licenses/by/3.0/",
+            }
+        self.assertEqual(package.metadata, expected_metadata)
+        
+    def test_makestyledepub(self):
+        return 0
         
         
     
