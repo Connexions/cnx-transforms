ebfc699b35474f4c8ef46594cdde222046c8f6ef
Michael Harrison <mwharrison@rice.edu>
2015-09-30 14:37:04 +0000

    use test data for testing epub task, remove extrenious code

--- cnxtransforms/tests/test_tasks.py
+++ cnxtransforms/tests/test_tasks.py
@@ -7,53 +7,24 @@
 # ###
 import os
 import unittest
-import zipfile
-import pdb
 
 EPUB = None
 CALLBACK = None
 
 here = os.path.abspath(os.path.dirname(__file__))
 TEST_DATA_DIR = os.path.join(here, 'data')
-
-class MakeEpub(unittest.TestCase):
-    def make_one(self, file):
-        from cnxepub import Package
-        return Package.from_file(file)
     
+class MakeEpub(unittest.TestCase):
     @property
     def target(self):
         from ..tasks import make_styled_epub
         return make_styled_epub
-            
-    def test_get_metadata(self):
-        package_filepath = os.path.join(
-            TEST_DATA_DIR, 'book',
-            "9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6.opf")
-        package = self.make_one(package_filepath)
-        
-        expected_metadata = {
-            'publisher': "Connexions",
-            'publication_message': u'Nueva Versión',
-            'identifier': "org.cnx.contents.9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6",
-            'title': "9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6",
-            'language': "en-us",
-            'publisher': "Connexions",
-            'license_text': "This work is shared with the public using the Attribution 3.0 Unported (CC BY 3.0) license.",
-            'license_url': "http://creativecommons.org/licenses/by/3.0/",
-            }
-        self.assertEqual(package.metadata, expected_metadata)
         
     def test_makestyledepub(self):
-        output_file = "my-epub.epub"
-        epub = os.path.join(
-            TEST_DATA_DIR, 'book',
-            "simultaneous-equations-1.1.epub")
-        styled_epub = self.target(epub, output_file, CALLBACK)
-        
-        
+        #sample epub directory
+        epub = os.path.join(TEST_DATA_DIR, 'book')
+        styled_epub = self.target(epub, CALLBACK)
     
-
 
 class MakePDF(unittest.TestCase):
     @property
