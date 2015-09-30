d17cac875ce40bdc2c2655d08589626d418d860a
Michael Harrison <mwharrison@rice.edu>
2015-09-30 16:34:43 +0000

    alter output paths

--- cnxtransforms/tasks.py
+++ cnxtransforms/tasks.py
@@ -15,8 +15,11 @@ import contextlib
 
 from cnxepub import EPUB
 
+####
+## Test data directory for output, this will be changed in the future.
+####
 here = os.path.abspath(os.path.dirname(__file__))
-TEST_DATA_DIR = os.path.join(here, 'data')
+TEST_DATA_DIR = os.path.join(here, 'tests/data')
 
 from .main import app
 
@@ -39,42 +42,43 @@ def make_styled_epub(epub, callback=None):
     
     python_executable = 'python'
     oerexports_dir = "/home/vagrant/production/oer.exports"
-    output_dir = "/home/vagrant/"
+    output_dir = TEST_DATA_DIR
     
     epub = EPUB.from_file(epub)
 
     # Create a temporary directory to work in...
     with make_temp_directory() as build_dir:
         # Acquire the collection's data in a collection directory format.
-        pkg_name = epub._packages[0].metadata['title']
+        uuid = epub._packages[0].metadata['title'].split("@")[0]
         version = epub._packages[0].metadata["title"].split("@")[1]
-        #collection_dir = os.path.join(pkg_name, version)
 
         # Run the oer.exports script against the collection data.
         build_script = os.path.join(oerexports_dir, 'content2epub.py')
-        result_filename = '{0}.epub'.format(pkg_name)
+        result_filename = '{0}-{1}.epub'.format(uuid, version)
         result_filepath = os.path.join(build_dir, result_filename)
+        output_filepath = os.path.join(output_dir, result_filename)
         
         epub.to_file(epub, result_filepath)
 
-        command = [python_executable, build_script, pkg_name,
+        command = [python_executable, build_script, uuid,
                    # The follow are not optional, values must be supplied.
                    '-t', 'collection',
                    '-c', os.path.join(oerexports_dir, 'static', 'content.css'),
                    '-e', os.path.join(oerexports_dir, 'xsl', 'dbk2epub.xsl'),
-                   '-o', result_filepath,
+                   '-o', output_filepath,
                    ]
         process = subprocess.Popen(command,
                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                    cwd=build_dir)
         stdout, stderr = process.communicate()
-        if process.returncode != 0:
+        
+        #if process.returncode != 0:
             # Something went wrong...
-            print "Something went wrong with the transform."
+        #    raise RuntimeError("Something went wrong with the transform.")
     
         # Move the file to it's final destination.
         shutil.copy2(result_filepath, output_dir)
-        output_filepath = os.path.join(output_dir, result_filename)
+        
 
     
     return [output_filepath]
