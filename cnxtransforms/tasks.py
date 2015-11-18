498fcc66058d31b8a1c6f901820f7a71f191b619
rich-hart <rich.hart@rice.edu>
2015-11-18 16:15:06 +0000

    using temp_dir contect handler

--- cnxtransforms/tasks.py
+++ cnxtransforms/tasks.py
@@ -93,66 +93,67 @@ def make_pdf(epub, callback=None):
     oerexports_dir = "/home/vagrant/production/oer.exports"
     output_dir = TEST_DATA_DIR
     pdf_generator_executable = '/usr/bin/prince'
-
     # Create a temporary directory to work in...
-    build_dir = tempfile.mkdtemp()
-    logger.debug("Working in '{0}'.".format(build_dir))
+    with make_temp_directory() as build_dir:
 
-    # Acquire the collection's data in a collection directory format.
-    pkg_name = build_request.get_package()
-    version = build_request.get_version()
-    base_uri = build_request.transport.uri
-    collection_dir = utils.get_offlinezip(pkg_name, version, base_uri,
-                                           build_dir)
-    collection_dir = os.path.join(build_dir, collection_dir, 'content')
-
-    # FIXME We need to grab the version from the unpacked directory
-    #       name because 'latest' is only a symbolic name that will
-    #       not be used in the resulting filename.
-    if version == 'latest':
-        # Using the unpacked complete zip filename, with the structure
-        #   <id>_<version>_complete, we can parse the version.
-        version = collection_dir.split('_')[1]
-
-    #Extract the print-style from the collection.xml
-    printstyle_xsl = os.path.join(oerexports_dir,'xsl','collxml-print-style.xsl')
-    collxml_path = os.path.join(collection_dir,'collection.xml')
-    xslt = etree.XSLT(etree.parse(printstyle_xsl))
-    collxml = etree.parse(collxml_path)
-    printstyle = str(xslt(collxml)).strip()
+        # Create a temporary directory to work in...
+        logger.debug("Working in '{0}'.".format(build_dir))
     
+        # Acquire the collection's data in a collection directory format.
+        pkg_name = build_request.get_package()
+        version = build_request.get_version()
+        base_uri = build_request.transport.uri
+        collection_dir = utils.get_offlinezip(pkg_name, version, base_uri,
+                                               build_dir)
+        collection_dir = os.path.join(build_dir, collection_dir, 'content')
     
-    # Run the oer.exports script against the collection data.
-    build_script = os.path.join(oerexports_dir, 'collectiondbk2pdf.py')
-    result_filename = '{0}-{1}.pdf'.format(build_request.get_package(),
-                                           version)
-    result_filepath = os.path.join(build_dir, result_filename)
-    command = [python_executable, build_script,
-               '-p', pdf_generator_executable,
-               '-d', collection_dir,
-               # XXX We need a place to input this option...
-               '-s', printstyle,
-               result_filepath,
-               ]
-    logger.debug("Running: " + ' '.join(command))
-    process = subprocess.Popen(command,
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                               cwd=build_dir)
-    stdout, stderr = process.communicate()
-    if process.returncode != 0:
-        # Something went wrong...
-        raise coyote.Failed("Unknown issue: \n" + stderr)
-        return
-    else:
-        msg = "PDF created, moving contents to final destination..."
-        logger.debug(msg)
-
-    # Move the file to it's final destination.
-    shutil.copy2(result_filepath, output_dir)
-    output_filepath = os.path.join(output_dir, result_filename)
-
-    # Remove the temporary build directory
-    shutil.rmtree(build_dir)
+        # FIXME We need to grab the version from the unpacked directory
+        #       name because 'latest' is only a symbolic name that will
+        #       not be used in the resulting filename.
+        if version == 'latest':
+            # Using the unpacked complete zip filename, with the structure
+            #   <id>_<version>_complete, we can parse the version.
+            version = collection_dir.split('_')[1]
+    
+        #Extract the print-style from the collection.xml
+        printstyle_xsl = os.path.join(oerexports_dir,'xsl','collxml-print-style.xsl')
+        collxml_path = os.path.join(collection_dir,'collection.xml')
+        xslt = etree.XSLT(etree.parse(printstyle_xsl))
+        collxml = etree.parse(collxml_path)
+        printstyle = str(xslt(collxml)).strip()
+        
+        
+        # Run the oer.exports script against the collection data.
+        build_script = os.path.join(oerexports_dir, 'collectiondbk2pdf.py')
+        result_filename = '{0}-{1}.pdf'.format(build_request.get_package(),
+                                               version)
+        result_filepath = os.path.join(build_dir, result_filename)
+        command = [python_executable, build_script,
+                   '-p', pdf_generator_executable,
+                   '-d', collection_dir,
+                   # XXX We need a place to input this option...
+                   '-s', printstyle,
+                   result_filepath,
+                   ]
+        logger.debug("Running: " + ' '.join(command))
+        process = subprocess.Popen(command,
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                   cwd=build_dir)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            # Something went wrong...
+            raise coyote.Failed("Unknown issue: \n" + stderr)
+            return
+        else:
+            msg = "PDF created, moving contents to final destination..."
+            logger.debug(msg)
+    
+        # Move the file to it's final destination.
+        shutil.copy2(result_filepath, output_dir)
+        output_filepath = os.path.join(output_dir, result_filename)
+    
+        # Remove the temporary build directory
+        shutil.rmtree(build_dir)
 
     return [output_filepath]
 
